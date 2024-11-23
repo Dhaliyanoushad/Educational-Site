ca03becafad7ffb8bc449fc0e58bfdc575d9ff23
Daliya <daliyadeutsch@gmail.com>
2024-11-23 15:31:59 +0530

    Blah Blah

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
 import Title from "./components/Title/Title";
 import About from "./components/About/About";
+import Campus from "./components/Campus/Campus";
+import Testi from "./components/Testimonials/Testi";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -15,6 +19,12 @@ const App = () => {
         <Programs />
         <About />
         <Title subtitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subtitle="Testimonials" title="What Student Says" />
+        <Testi />
+        <Title subtitle="Contact Us" title="Get in touch" />
+        <Contact />
+        <Footer />
       </div>
     </div>
   );
