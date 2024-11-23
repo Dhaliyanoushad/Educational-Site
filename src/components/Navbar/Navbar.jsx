c6de77de85f00bb66d2364d6d2dbe3ec665a67fc
Daliya <daliyadeutsch@gmail.com>
2024-11-23 15:32:16 +0530

    LINK react-scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -17,13 +18,41 @@ const Navbar = () => {
     >
       <img src={logo} alt="" className="w-[180px]" />
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
         <li>
-          <button className="btn">Contact Us</button>
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="program" smooth={true} offset={-260} duration={500}>
+            Program
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} offset={-150} duration={500}>
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link to="campus" smooth={true} offset={-260} duration={500}>
+            Campus
+          </Link>
+        </li>
+        <li>
+          <Link to="testi" smooth={true} offset={-260} duration={500}>
+            Testimonials
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="btn"
+            to="contact"
+            smooth={true}
+            offset={-260}
+            duration={500}
+          >
+            Contact Us
+          </Link>
         </li>
       </ul>
     </nav>
